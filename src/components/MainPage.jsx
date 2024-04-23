c2fb51fdae76e9b5b2b7b73302ce523b02817c2b
vinunarwal <vinunarwal3@gmail.com>
2024-04-23 11:51:22 -0500

    ..

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './Home'
 import MidHeader from './MidHeader'
 import ColorPicker from './ColorPicker'
+import Footer from './Footer'
 
 function MainPage() {
   return (
