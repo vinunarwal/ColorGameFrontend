82b1bc4560492a3ecf4b30b9e43d628526297bc9
SahilSheorn <sahilks0317@gmail.com>
2024-04-29 21:18:18 +0530

    NEW CODE

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,22 +7,22 @@ function Home() {
   const [isOpen, setIsOpen] = useState(false);
   const [bankBalance, setBankBalance] = useState("0");
 
-  useEffect(()=>{
+  useEffect(() => {
     const token = localStorage.getItem('token');
-      if (token) {
-         const decodedToken = jwtDecode(token);
+    if (token) {
+      const decodedToken = jwtDecode(token);
 
-         axios.get(`http://localhost:5000/user/${decodedToken.userId}`)
-         .then(response => {
+      axios.get(`http://localhost:5000/user/${decodedToken.userId}`)
+        .then(response => {
           setBankBalance(response.data.bankBalance);
-         })
-         .catch(error => {
-            console.error('Error fetching user data:', error);
-         });
-     
-      }
-  } ,[]
-);
+        })
+        .catch(error => {
+          console.error('Error fetching user data:', error);
+        });
+
+    }
+  }, []
+  );
 
   const openPopup = () => {
     setIsOpen(true);
@@ -77,22 +77,24 @@ function Home() {
               )}
             </div>
             <div className="mt-4">
-              <button className="bg-white text-black py-2 px-3 rounded-full">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-                  />
-                </svg>
-              </button>
+              <Link to="/">
+                <button className="bg-white text-black py-2 px-3 rounded-full">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                    />
+                  </svg>
+                </button>
+              </Link>
             </div>
           </div>
         </div>
